d20bfc3fed64b10ef90566d01bd5442bbf9dd2e6
st3pbyst3p <cornel.scoarta@gmail.com>
2018-04-19 16:25:30 +0300

    changed visibility of navbars when mobile device

---
+++ src/core/eeh-navigation-button-provider.js
@@ -0,0 +1,14 @@
+angular.module('eehNavigation').factory('buttonState', function(){
+    
+          var btnStateVar = true; //initial - true for hidden
+          return {
+            "changeState": function(){ //toggle the visibility
+              btnStateVar = !btnStateVar;
+              return btnStateVar;
+            },
+            "btnState": function(){ //check the current visibility
+              return btnStateVar;
+            }
+          } 
+      
+});
