e620903ab2bb3a66ab484d089aa3febba96921b1
st3pbyst3p <cornel.scoarta@gmail.com>
2022-11-14 16:51:04 +0200

    alt: fix css

--- dist/eeh-navigation.tpl.js
+++ dist/eeh-navigation.tpl.js
@@ -11,7 +11,7 @@ angular.module('eehNavigation').run(['$templateCache', function($templateCache) 
     "<i class=\"fa fa-star-o pull-right\" style=\"font-size: 15px; margin-right: 5px; width: 1.28571429em; text-align: center;\" aria-hidden=\"true\" ng-if=\"altCanBeFavorites(menuItem) && !menuItem.isFavorite && !menuItem.iconClass\" ng-click=\"altManageFavorites(menuItem)\"></i>\n" +
     "<i class=\"fa fa-star pull-right\" style=\"font-size: 15px; margin-right: 5px; width: 1.28571429em; text-align: center; color: #337ab7;\" aria-hidden=\"true\" ng-if=\"altCanBeFavorites(menuItem) && menuItem.isFavorite && !menuItem.iconClass\" ng-click=\"altManageFavorites(menuItem)\"></i>\n" +
     "<span class=\"menu-item-icon icon-fw fa fa-star-o pull-right\" style=\"font-size: 15px;\" aria-hidden=\"true\" ng-if=\"altCanBeFavorites(menuItem) && !menuItem.isFavorite && menuItem.iconClass\" ng-click=\"altManageFavorites(menuItem)\"></span>\n" +
-    "<span class=\"menu-item-icon icon-fw fa fa-star pull-right\" style=\"font-size: 15px;\" aria-hidden=\"true\" ng-if=\"altCanBeFavorites(menuItem) && menuItem.isFavorite && menuItem.iconClass\" ng-click=\"altManageFavorites(menuItem)\"></span>"
+    "<span class=\"menu-item-icon icon-fw fa fa-star pull-right\" style=\"font-size: 15px; color: #337ab7;\" aria-hidden=\"true\" ng-if=\"altCanBeFavorites(menuItem) && menuItem.isFavorite && menuItem.iconClass\" ng-click=\"altManageFavorites(menuItem)\"></span>"
   );
 
 
